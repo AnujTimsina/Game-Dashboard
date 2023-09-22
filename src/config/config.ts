b0812d7d19ef0b260a3c3e52a60d41527464eb8c
Sandip Thapa <Sandip@metapolis.studio>
2023-09-22 18:40:53 +0545

    fix: shifted server_url to env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = 'https://api.prajwalchhetri.com.np/v1';
-export const BACKEND_URL = 'http://localhost:3000/v1';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
